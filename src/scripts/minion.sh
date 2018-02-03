fb851eee9a98292f06b38306aea3960de13164e8
Victor Varza <victor.varza@gmail.com>
2018-02-03 15:52:37 +0800

    upgrade to k8s v1.13.4

--- src/scripts/minion.sh
+++ src/scripts/minion.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+source ./src/scripts/vars
 cluster_join=""
 
 while [ "$cluster_join" == "" ] ; do
@@ -9,3 +10,12 @@ while [ "$cluster_join" == "" ] ; do
 done
 
 $cluster_join 
+
+if [[ "$NETWORK_PLUGIN" == "cilium" ]]; then
+## setup kubelet
+cat <<EOF >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+ExecStartPre=/bin/bash -c "if [[ $(/bin/mount | /bin/grep /sys/fs/bpf -c) -eq 0 ]]; then /bin/mount bpffs /sys/fs/bpf -t bpf; fi"
+EOF
+systemctl daemon-reload
+systemctl restart kubelet
+fi
--- src/template/pack.sh
+++ src/template/pack.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+rm -rf output/k8s-base.box 2> /dev/null
 vagrant package --output output/k8s-base.box 
 vagrant box add output/k8s-base.box --name qedzone/k8s-base
