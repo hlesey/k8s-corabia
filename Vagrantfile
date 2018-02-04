ea10a8eda0febce7cb8d5fb52c64d1f2d6e2a937
Victor Varza <victor.varza@gmail.com>
2018-02-03 21:55:40 -0800

    upgrade to k8s v1.14.1

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,7 @@
 # Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
 VAGRANTFILE_API_VERSION = "2"
 BOX_IMAGE="qedzone/k8s-base"
-BOX_VERSION="1.13.4"
+BOX_VERSION="1.14.1"
 
 required_plugins = %w(vagrant-vbguest vagrant-share)
 
@@ -23,7 +23,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     config.vm.define hostname do |cfg|
       cfg.vm.provider :virtualbox do |vb, override|
         config.vm.box = BOX_IMAGE
-        config.vm.box_version = BOX_VERSION
+        # config.vm.box_version = BOX_VERSION
         override.vm.network :private_network, ip: "#{info[:ip]}"
         override.vm.hostname = hostname + ".local"
 
--- src/template/setup.sh
+++ src/template/setup.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
 
 # https://kubernetes.io/docs/setup/independent/install-kubeadm/
-
-K8S_VERSION="1.13.4"
+K8S_VERSION="1.14.1"
 
 ############################### INITIAL SETUP ###############################
+# update system 
 apt-get update && apt-get upgrade -y
 apt-get install -y apt-transport-https curl telnet 
-
-# config vim
-cat <<EOF >/root/.vimrc
-syntax on
-filetype on
-set incsearch
-set ignorecase
-set number
-set tabstop=2 smarttab expandtab
-set shiftwidth=2
-EOF
 
 # add kubernetes repos
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
@@ -33,25 +22,39 @@ systemctl enable docker
 # install kubeadm
 apt-get install -y kubeadm=${K8S_VERSION}-00 kubelet=${K8S_VERSION}-00 kubectl=${K8S_VERSION}-00
 
-# other
+# fixes
+## configure utf-8
 cat <<EOF > /etc/environment
 LANG=en_US.utf-8
 LC_ALL=en_US.utf-8
 EOF
 
+## config vim
+cat <<EOF >/root/.vimrc
+syntax on
+filetype on
+set incsearch
+set ignorecase
+set number
+set tabstop=2 smarttab expandtab
+set shiftwidth=2
+EOF
+
+
 ############################### KERNEL UPGRADE ###############################
 # needed for cilium bpf
-KERNEL="v4.19.25"
-KERNEL_LONG="4.19.25-041925"
-KERNEL_DATE="201902230535"
+# KERNEL="v4.19.25"
+# KERNEL_LONG="4.19.25-041925"
+# KERNEL_DATE="201902230535"
 
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/${KERNEL}/linux-headers-${KERNEL_LONG}_${KERNEL_LONG}.${KERNEL_DATE}_all.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/${KERNEL}/linux-headers-${KERNEL_LONG}-generic_${KERNEL_LONG}.${KERNEL_DATE}_amd64.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/${KERNEL}/linux-image-unsigned-${KERNEL_LONG}-generic_${KERNEL_LONG}.${KERNEL_DATE}_amd64.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/${KERNEL}/linux-modules-${KERNEL_LONG}-generic_${KERNEL_LONG}.${KERNEL_DATE}_amd64.deb
+# wget http://kernel.ubuntu.com/~kernel-ppa/mainline/${KERNEL}/linux-headers-${KERNEL_LONG}_${KERNEL_LONG}.${KERNEL_DATE}_all.deb
+# wget http://kernel.ubuntu.com/~kernel-ppa/mainline/${KERNEL}/linux-headers-${KERNEL_LONG}-generic_${KERNEL_LONG}.${KERNEL_DATE}_amd64.deb
+# wget http://kernel.ubuntu.com/~kernel-ppa/mainline/${KERNEL}/linux-image-unsigned-${KERNEL_LONG}-generic_${KERNEL_LONG}.${KERNEL_DATE}_amd64.deb
+# wget http://kernel.ubuntu.com/~kernel-ppa/mainline/${KERNEL}/linux-modules-${KERNEL_LONG}-generic_${KERNEL_LONG}.${KERNEL_DATE}_amd64.deb
 
-dpkg -i *.deb
-rm -rf *.deb
+# dpkg -i *.deb
+# rm -rf *.deb
+
 
 ############################### PREPARE KUBEADM ###############################
 kubeadm config images pull
