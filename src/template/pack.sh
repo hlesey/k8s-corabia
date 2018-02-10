2c65b070eb3b3e516507f94c21be6a45cbfeb964
Victor Varza <victor.varza@gmail.com>
2018-02-10 13:27:02 -0800

    update to k8s v1.15.1 version

--- src/template/pack.sh
+++ src/template/pack.sh
@@ -2,5 +2,5 @@
 
 rm -rf output/k8s-base.box 2> /dev/null
 vagrant package --output output/k8s-base.box 
-vagrant box remove qedzone/k8s-base  2> /dev/null
+vagrant box remove hlesey/k8s-base  2> /dev/null
 vagrant box add output/k8s-base.box --name hlesey/k8s-base
