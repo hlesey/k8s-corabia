fc7dc054a9f5d240c23207b5f8c3c961f8e8c32d
Victor Varza <victor.varza@gmail.com>
2018-02-10 19:22:15 +0800

    fix v1.14.1

--- src/scripts/common.sh
+++ src/scripts/common.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+source /src/scripts/vars
+
 # configure /etc/hosts file
-echo "192.168.100.100   master master.local nfsserver.local" >> /etc/hosts
-echo "192.168.100.101   node01 node01.local" >> /etc/hosts
-echo "192.168.100.102   node02 node02.local" >> /etc/hosts
-echo "192.168.100.103   node03 node03.local" >> /etc/hosts
+echo "$MASTER_IP   master master.local nfsserver.local" >> /etc/hosts
+echo "$NODE01_IP   node01 node01.local"                 >> /etc/hosts
+echo "$NODE02_IP   node02 node02.local"                 >> /etc/hosts
+echo "$NODE03_IP   node03 node03.local"                 >> /etc/hosts
