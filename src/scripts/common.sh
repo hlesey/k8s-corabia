dcde4ad21e4529e90a6612caa68b8dd8a9be02a7
Victor Varza <victor.varza@gmail.com>
2018-02-24 14:52:49 +0800

    use external dns instead of vbox

--- src/scripts/common.sh
+++ src/scripts/common.sh
@@ -8,9 +8,16 @@ echo "$NODE01_IP   node01 node01.local"                 >> /etc/hosts
 echo "$NODE02_IP   node02 node02.local"                 >> /etc/hosts
 echo "$NODE03_IP   node03 node03.local"                 >> /etc/hosts
 
+# configure external DNS, instead of using VBox DNS
+# systemd-resolve --status
+echo "DNS=8.8.8.8" >> /etc/systemd/resolved.conf
+echo "DNS=8.8.4.4" >> /etc/systemd/resolved.conf
+systemctl restart systemd-resolved
+
 
 #########################################################
-### fix docker driver --> should be moved in template box
+### should be moved in template box
+### fix docker driver
 # Setup daemon.
 cat > /etc/docker/daemon.json <<EOF
 {
@@ -32,7 +39,8 @@ systemctl restart docker
 
 
 #########################################################
-### fix sshd dns lookup --> should be moved in template box
+### should be moved in template box
+### fix sshd dns lookup
 echo "UseDNS no" >> /etc/ssh/sshd_config
 systemctl restart sshd
 #########################################################
