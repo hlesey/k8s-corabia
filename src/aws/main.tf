1664c1042baf6fe6e457798a4a8a2b9d96c9f8bc
Victor Varza <victor.varza@gmail.com>
2019-02-23 07:57:05 +0800

    remove base template

--- src/aws/main.tf
+++ src/aws/main.tf
@@ -10,16 +10,35 @@ resource "aws_key_pair" "main" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block            = "192.168.234.0/24"
+  cidr_block            = "192.168.0.0/16"
   enable_dns_hostnames  = true
 }
 
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "192.168.234.0/24"
-  availability_zone = "${var.region}${var.az}"
+resource "aws_internet_gateway" "gw" {
+    vpc_id = aws_vpc.main.id
 }
 
+resource "aws_route_table" "r" {
+    vpc_id = aws_vpc.main.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gw.id
+    }
+
+    depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route_table_association" "main" {
+    subnet_id = aws_subnet.main.id
+    route_table_id = aws_route_table.r.id
+}
+
+resource "aws_subnet" "main" {
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = "192.168.234.0/24"
+    availability_zone = "${var.region}${var.az}"
+    map_public_ip_on_launch = true
+}
 
 resource "aws_security_group" "kubernetes" {
   name          = var.cluster-name
@@ -91,4 +110,5 @@ resource "aws_spot_instance_request" "control-plane" {
     subnet_id                   = aws_subnet.main.id
     associate_public_ip_address = true
     vpc_security_group_ids      = [aws_security_group.kubernetes.id]
+    depends_on                  = [aws_internet_gateway.gw, aws_key_pair.main]
 }
--- src/scripts/control-plane.sh
+++ src/scripts/control-plane.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 # Setup and bootstrap k8s control-plane 
+set -xe
 
 source /src/scripts/vars.txt
 
-# init the control plane components
-kubeadm init --apiserver-advertise-address=$CONTROL_PLANE_IP --pod-network-cidr=10.244.0.0/16 > /src/output/.kubeadmin_init
-
-# tba with kubeadm configfile:
-# cat "/src/manifests/kubeadm/cluster.yaml" | sed -e "s'{{CONTROL_PLANE_IP}}'${CONTROL_PLANE_IP}'g"  | kubeadm init --config -
+# bootstrap k8s control-plane components
+cat /src/manifests/kubeadm/control-plane.yaml | sed -e "s'{{CONTROL_PLANE_IP}}'${CONTROL_PLANE_IP}'g"  > /tmp/control-plane.yaml
+kubeadm init --config /tmp/control-plane.yaml > /src/output/.kubeadmin_init
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
@@ -18,12 +17,15 @@ else
     kubectl apply -f /src/manifests/network/${NETWORK_PLUGIN}
 fi
 
-# workaround for 140615704306112:error:2406F079:random number generator:RAND_load_file:Cannot open file:../crypto/rand/randfile.c:88:Filename=/root/.rnd
-touch /root/.rnd && chmod 600 /root/.rnd
-touch /home/vagrant/.rnd && chmod 600 /home/vagrant/.rnd && chown vagrant:vagrant /home/vagrant/.rnd
+# # workaround for 140615704306112:error:2406F079:random number generator:RAND_load_file:Cannot open file:../crypto/rand/randfile.c:88:Filename=/root/.rnd
+# touch /root/.rnd && chmod 600 /root/.rnd
+# touch /home/vagrant/.rnd && chmod 600 /home/vagrant/.rnd && chown vagrant:vagrant /home/vagrant/.rnd
 
 # deploy dashboard
-source /src/scripts/dashboard.sh
+kubectl apply -f /src/manifests/dashboard/
+
+# setup cluster-admin sa
+kubectl apply -f /src/manifests/rbac/rbac.yaml
 
 # deploy ingress controller
 kubectl apply -f  /src/manifests/ingress/${INGRESS_CONTROLLER}
@@ -41,11 +43,6 @@ cp /etc/kubernetes/admin.conf /src/output/kubeconfig.yaml
 # configure vagrant and root user with kubeconfig
 echo "export KUBECONFIG=/src/output/kubeconfig.yaml"  >> /root/.bashrc
 echo "export KUBECONFIG=/src/output/kubeconfig.yaml"  >> /home/vagrant/.bashrc
-
-# Enabling shell autocompletion -> FIXME: add them in the image template
-echo "source <(kubectl completion bash)" >> /root/.bashrc
-echo ". /usr/share/bash-completion/bash_completion" >> /root/.bashrc
-echo  "alias kns='kubectl config set-context \$(kubectl config current-context) --namespace'" >>  /root/.bashrc
 
 # copy root user bash config to vagrant user
 cat /root/.bashrc >> /home/vagrant/.bashrc
