2edf6e7ae41b842454f980e9d5105af2ed14f9ff
Victor <victor@laptop.local>
2019-10-07 11:00:23 +0300

    fixes

--- src/scripts/common.sh
+++ src/scripts/common.sh
@@ -7,3 +7,25 @@ echo "$MASTER_IP   master master.local nfsserver.local" >> /etc/hosts
 echo "$NODE01_IP   node01 node01.local"                 >> /etc/hosts
 echo "$NODE02_IP   node02 node02.local"                 >> /etc/hosts
 echo "$NODE03_IP   node03 node03.local"                 >> /etc/hosts
+
+
+#########################################################
+### fix docker driver --> should be moved in template box
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker.
+systemctl daemon-reload
+systemctl restart docker
+#########################################################
