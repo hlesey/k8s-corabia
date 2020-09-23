b3d139d2e4eb204d712b5e7705791038b0cf3ef0
Victor Varza <victor.varza@gmail.com>
2020-09-23 14:22:25 +0300

    add prerequisites docs

---
+++ src/scripts/download_boxes.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+UBUNTU_BOX_IMAGE="ubuntu/bionic64"
+UBUNTU_BOX_VERSION="20200429.0.0"
+
+KUBERNETES_BOX_IMAGE="hlesey/k8s-base"
+KUBERNETES_BOX_VERSION="1.18.2.1"
+
+# ubuntu
+vagrant box add $UBUNTU_BOX_IMAGE --box-version $UBUNTU_BOX_VERSION --provider virtualbox
+
+# kubernetes
+vagrant box add $KUBERNETES_BOX_IMAGE --box-version $KUBERNETES_BOX_VERSION --provider virtualbox
