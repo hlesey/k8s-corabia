360c696e3ca60e9274a58dca4097fb8dc55fe90e
Adrian Aneci <aneci@adobe.com>
2021-03-15 11:49:57 +0200

    Make kubeconfig workable from local and add cluster-admin-token as output

--- src/aws/output.tf
+++ src/aws/output.tf
@@ -11,6 +11,7 @@ output "cluster_info" {
       control-plane-public-ip : name.control_plane_public_ip
       control-plane-public-dns : name.control_plane_public_dns
       kubeconfig: name.kubeconfig
+      cluster-admin-token: name.cluster-admin-token
     }
   ]
 }
