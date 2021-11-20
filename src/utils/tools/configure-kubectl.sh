6f3fc194ec002c27b3102518bea86b264bd80ca2
adriananeci <aneci.adrian@gmail.com>
2021-11-20 13:42:58 +0200

    Fix permissions and env path (#42)
    * Fix permissions and env path
    
    * Adjust cloud-prerequisites.md

--- src/utils/tools/configure-kubectl.sh
+++ src/utils/tools/configure-kubectl.sh
@@ -4,6 +4,7 @@ WINDOWS="MINGW64_NT"
 MAC="Darwin"
 
 extension=""
+env_path="$HOME/.bash_profile"
 
 if [[ $(uname | grep $WINDOWS) != "" ]]; then
     os="windows"
@@ -12,6 +13,9 @@ elif [[ $(uname| grep $MAC) != "" ]]; then
 	os="mac"
 else
 	os="linux"
+	# If bash is not opened with interactive login, .bash_profile won't be loaded
+	# See https://askubuntu.com/a/121075 for a good explanation
+	env_path="$HOME/.profile"
 fi
 
 echo "Detected $os operating system."
@@ -19,8 +23,9 @@ echo "Detected $os operating system."
 mkdir -p bin
 curl -o bin/kubectl -LO "https://dl.k8s.io/release/$(curl -sL https://dl.k8s.io/release/stable.txt)/bin/$os/amd64/kubectl${extension}"
 curl -o bin/kubetail -LO https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail
+chmod +x bin/kube*
 
-cat <<EOF >> ~/.bash_profile
+cat <<EOF >> "${env_path}"
 export PATH="$PATH:$(pwd)/bin"
 export KUBECONFIG="$(pwd)/kubeadm-vagrant/src/output/kubeconfig.yaml:$(pwd)/config/kubeconfig.yaml"
 alias kns='kubectl config set-context \$(kubectl config current-context) --namespace'
