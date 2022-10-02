c410c665bc1bffc5263bdaa51f6510705df23ade
Victor Varza <victor.varza@gmail.com>
2022-10-02 17:04:32 +0300

    Fix kubectl for macos (#53)

--- src/utils/tools/configure-kubectl.sh
+++ src/utils/tools/configure-kubectl.sh
@@ -8,14 +8,11 @@ env_path="$HOME/.bash_profile"
 
 if [[ $(uname | grep $WINDOWS) != "" ]]; then
     os="windows"
-	core_os="windows"
     extension=".exe"
 elif [[ $(uname| grep $MAC) != "" ]]; then
-	os="mac"
-	core_os="darwin"
+	os="darwin"
 else
 	os="linux"
-	core_os="linux"
 	# If bash is not opened with interactive login, .bash_profile won't be loaded
 	# See https://askubuntu.com/a/121075 for a good explanation
 	env_path="$HOME/.bashrc"
@@ -26,7 +23,7 @@ echo "Detected $os operating system."
 mkdir -p bin
 curl -o bin/kubectl -LO "https://dl.k8s.io/release/$(curl -sL https://dl.k8s.io/release/stable.txt)/bin/${os}/amd64/kubectl${extension}"
 curl -o bin/kubetail -LO https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail
-curl -o bin/hey -LO "https://hey-release.s3.us-east-2.amazonaws.com/hey_${core_os}_amd64"
+curl -o bin/hey -LO "https://hey-release.s3.us-east-2.amazonaws.com/hey_${os}_amd64"
 chmod +x bin/*
 
 cat <<EOF >> "${env_path}"
