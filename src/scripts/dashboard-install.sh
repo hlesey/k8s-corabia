c83afbbc583b18cb0e814ae76e997453e19e3cb1
Victor Varza <victor.varza@gmail.com>
2024-10-14 13:07:39 +0300

    fix dashboard ingress fqdn

--- src/scripts/dashboard-install.sh
+++ src/scripts/dashboard-install.sh
@@ -7,14 +7,18 @@ source /src/scripts/envs.sh
 ## deploy ingress controller custom objects
 kubectl apply -f  /src/manifests/ingress/nginx/custom.yaml
 
-# deploy dashboard helm chart
-sed -i -e "s'clusterx.qedzone.ro'${CONTROL_PLANE_PUBLIC_EXTERNAL_DNS}'g" /src/manifests/dashboard/helm-values.yaml
-
 # Add kubernetes-dashboard repository
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 helm repo update
-# Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart
-helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard --version "${DASHBOARD_VERSION}" -f /src/manifests/dashboard/helm-values.yaml
+
+# Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart.
+# This will deploy the ingress-nginx controller as well in the ingress-nginx namespace.
+helm upgrade \
+    --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
+    --create-namespace --namespace kubernetes-dashboard \
+    --version "${DASHBOARD_VERSION}" \
+    --set app.ingress.hosts[0]="${CONTROL_PLANE_PUBLIC_EXTERNAL_DNS}" \
+    -f /src/manifests/dashboard/helm-values.yaml
 
 ## deploy dashboard custom objects
 kubectl apply -f /src/manifests/dashboard/custom.yaml
